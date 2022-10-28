b6e803e720cd2d12745a0ebda608e2805a9a884e
FabianVladmir <49292220+FabianVladmir@users.noreply.github.com>
2022-10-28 10:43:54 -0500

    Time: 62 ms (70.98%), Space: 18.5 MB (93.61%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -7,20 +7,41 @@ public:
             return ans;
         
         
-        unordered_map<string, vector<string>> mp;
+        unordered_map<string, vector<string>> m;
         
-        for(auto str:strs) {
-            string temp = str;
-            sort(temp.begin(), temp.end());
-            mp[temp].push_back(str);
+        for(auto &it:strs){
+            int freq[26] = {0};    
+            for(auto i : it)            
+                freq[i-'a']++;
+            
+            string currStr;
+        
+            for(int i = 0; i<26; i++) {
+
+                if(freq[i] > 0) {
+                    currStr += string(freq[i], i+'a');
+                }
+            }
+            
+            m[currStr].push_back(it);
+            
+            //found
+            // if(m.find(currStr) != m.end()){
+            //     m[currStr].push_back(it);
+            // }
+            // //not found
+            // else{
+            //     m[currStr] = {it};
+            // }
+            
         }
         
-        
-        for(auto it : mp) {
-            ans.push_back(it.second);
+        for(auto &it : m) {
+            ans.push_back(std::move(it.second));
         }
         
         return ans;
+        
         
         
         
